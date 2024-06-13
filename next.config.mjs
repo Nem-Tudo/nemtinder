b27ac2e512b54251a9b1252becea61cb07aa5169
Nem Tudo <gf98289@gmail.com>
2024-06-12 23:11:26 -0300

    premium redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/premium',
+        destination: 'https://pixcord.shop/1112429999157428446?p=NemTinder%20Premium',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
