e62f782fea0354767d63c79b21e4e4e5e3e79ca6
Nem Tudo <gf98289@gmail.com>
2024-06-16 23:39:44 -0300

    fix: domain

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
-    // apiURL: "https://api.nemtinder.nemtudo.me",
-    apiURL: "http://localhost:3100",
+    apiURL: "https://api.nemtinder.nemtudo.me",
+    // apiURL: "http://localhost:3100",
     captchaKey: "6Ldqy_UpAAAAAB7eJ1DeayyW0JJ-X1N1CzefOAsz",
     vapidKey: "BG0lcMqbez6_4guF06uCoY6m8lawmwnigg7n62XwTJYOSAH6f2cQRlwNqEqTQmazTvSxkjJG9AtpOOQSq_z0B6I"
 }
