4cab326a1e97eb9f78fa17380830f151ceb98362
Nem Tudo <gf98289@gmail.com>
2025-02-08 16:56:03 -0300

    change api url

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 export default {
-    apiURL: "https://api.nemtinder.nemtudo.me",
+    apiURL: "https://apinemtinder.nemtudo.me",
     // apiURL: "http://localhost:3100",
     captchaKey: "6Ldqy_UpAAAAAB7eJ1DeayyW0JJ-X1N1CzefOAsz",
     vapidKey: "BG0lcMqbez6_4guF06uCoY6m8lawmwnigg7n62XwTJYOSAH6f2cQRlwNqEqTQmazTvSxkjJG9AtpOOQSq_z0B6I"
